d54734551504d4048a4bcc6b66a01d3885516855
Dirza1 <jasper.olthof@xs4all.nl>
2025-03-07 16:24:13 +0100

    fully implemented the setUp method

--- src/test_HTMLNode.py
+++ src/test_HTMLNode.py
@@ -13,7 +13,8 @@ class TestHTMLNode(unittest.TestCase):
         self.note_props = HTMLNode("a", "Google", None, props)
         self.note_empty = HTMLNode(None, None, None, None)
     
-    
+        self.leaf_p= LeafNode("p", "Hello, world!")
+        self.leaf_a = LeafNode("a", "click me!", {"href": "https://www.google.com"})
     def test_props_to_html_none(self):
         # Test without properties
         test_case = self.note.props_to_html()
@@ -21,8 +22,6 @@ class TestHTMLNode(unittest.TestCase):
 
     def test_props_to_html_full(self):
         # Test with properties
-        
-        
         result = self.note_props.props_to_html()
     
         # Since dictionary order is not guaranteed, we need to check for both possibilities
@@ -33,8 +32,8 @@ class TestHTMLNode(unittest.TestCase):
         self.assertIn(result, possible_outputs)
     
     def test_repr_simp(self):
-        test_case = self.note.props_to_html()       #testing the __repr__ function
-        expected_result = "HTMLNode: p, this is a test, None, None"
+        test_case = self.note_props      #testing the __repr__ function
+        expected_result = "HTMLNode: a, Google, None, {'href': 'https://www.google.com', 'target': '_blank'}"
         result = test_case.__repr__()
         self.assertEqual(result,expected_result)
     
@@ -64,19 +63,19 @@ class TestHTMLNode(unittest.TestCase):
             test.to_html()
 
     def test_constructor(self):
-        test = HTMLNode()
-        expected_restult = "HTMLNode: None, None, None, None"
+        test = self.note_empty
+        expected_result = "HTMLNode: None, None, None, None"
         result = test.__repr__()
-        self.assertEqual(result,expected_restult)
+        self.assertEqual(result,expected_result)
 
 
     def test_leaf_to_html_p(self):
-        node = LeafNode("p", "Hello, world!")
-        self.assertEqual(node.to_html(), "<p>Hello, world!</p>")
+        self.assertEqual(self.leaf_p.to_html(), "<p>Hello, world!</p>")
     
     def test_leaf_to_html_a(self):
-        node = LeafNode("a", "click me!", {"href": "https://www.google.com"})
-        self.assertEqual(node.to_html(), '<a href="https://www.google.com">click me!</a>')
+        self.assertEqual(self.leaf_a.to_html(), '<a href="https://www.google.com">click me!</a>')
+
+
 
 if __name__ == "__main__":
     unittest.main()
