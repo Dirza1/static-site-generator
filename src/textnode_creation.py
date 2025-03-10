72b93695e72f03fc3b9f115de57a05ce6ae87e18
jasper.olthof <jasper.olthof@thermofisher.com>
2025-03-10 12:20:24 +0100

    Continuation on debuggung textnodecreation

--- src/textnode_creation.py
+++ src/textnode_creation.py
@@ -13,13 +13,12 @@ def split_nodes_delimiter(old_nodes, delimiter,text_type):
     to_return_list = []
     for node in old_nodes_split:
         if node.startswith(delimiter) and node.endswith(delimiter):
-            new_node = TextNode(node,text_type)
-            to_return_list.extend(new_node)
+            new_node = TextNode(node,text_type) 
         if node.startswith(delimiter) or node.endswith(delimiter):
             raise Exception("invalid markdown text. Not all delimiters have a matching set")
         else:
             new_node = TextNode(node,TextType.TEXT)
-
+        to_return_list.append(new_node)
     return to_return_list
 
 main()
