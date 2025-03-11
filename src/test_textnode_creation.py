97a6088faea4c6021fab9d61acab151afc189482
Dirza1 <jasper.olthof@xs4all.nl>
2025-03-11 08:43:33 +0100

    kinda stuck tbh

--- src/test_textnode_creation.py
+++ src/test_textnode_creation.py
@@ -6,12 +6,12 @@ from textnode import *
 class TestTextNideCreation(unittest.TestCase):
 
     def setUp(self):
-        self.text_node_test = TextNode("This is text with a `code block` word", TextType.TEXT)
-
+        self.text_node_test1 = TextNode("This is text with a `code block` word", TextType.TEXT)
+        self.text_node_test2 = TextNode("This is text with a `code block` word", TextType.TEXT)
 
 
     def test_simple(self):
-       result = split_nodes_delimiter(self.text_node_test,"`",TextType.CODE)
+       result = split_nodes_delimiter([self.text_node_test1,],"`", TextType.CODE)
 
        self.assertEqual(len(result), 3)
        self.assertEqual(result[0].text, "This is text with a ")
--- src/textnode_creation.py
+++ src/textnode_creation.py
@@ -2,15 +2,42 @@ from htmlnode import *
 from textnode import *
 
 def split_nodes_delimiter(old_nodes, delimiter, text_type):
-    old_nodes_split = old_nodes.text.split(delimiter)
-    to_return_list = []
-    for node in old_nodes_split:
-        if node.startswith(delimiter) and node.endswith(delimiter):
-            new_node = TextNode(node,text_type)
-            to_return_list.extend(new_node)
-        if node.startswith(delimiter) or node.endswith(delimiter):
-            raise Exception("invalid markdown text. Not all delimiters have a matching set")
-        else:
-            new_node = TextNode(node,TextType.TEXT)
+    result = []
+    
+    for old_node in old_nodes:
+        # If not a TEXT node, add it unchanged
+        if old_node.text_type != TextType.TEXT:
+            result.append(old_node)
+            continue
+            
+        # Process text nodes
+        text = old_node.text
+        remaining_text = text
+        current_position = 0
+        
+        # Continue until we've processed the entire string
+        while current_position < len(text):
+            # Find the next opening delimiter
+            opening_index = text.find(delimiter, current_position)
+            
+            if opening_index == -1:
+                # No more delimiters, add any remaining text
+                if current_position < len(text):
+                    result.append(TextNode(text[current_position:], TextType.TEXT))
+                break
+                
+            # Add text before the delimiter if there is any
+            if opening_index > current_position:
+                result.append(TextNode(text[current_position:opening_index], TextType.TEXT))
+                
+            # Find the closing delimiter
+            closing_index = text.find(delimiter, opening_index + len(delimiter))
+            if closing_index == -1:
+                raise Exception(f"No closing delimiter '{delimiter}' found")
+                
+            # Add the delimited content with the special type
+            delimited_content = text[opening_index + len(delimiter):closing_index]
+            result.append(TextNode(delimited_content, text_type))
 
-    return to_return_list
+
+    return result
