3583cdbc88221b7aaa7c27b315932f1b8a5fe2cb
Dirza1 <jasper.olthof@xs4all.nl>
2025-03-14 08:52:31 +0100

    still stuck, just aa bit further along

--- src/textnode_creation.py
+++ src/textnode_creation.py
@@ -12,7 +12,6 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             
         # Process text nodes
         text = old_node.text
-        remaining_text = text
         current_position = 0
         
         # Continue until we've processed the entire string
@@ -38,6 +37,8 @@ def split_nodes_delimiter(old_nodes, delimiter, text_type):
             # Add the delimited content with the special type
             delimited_content = text[opening_index + len(delimiter):closing_index]
             result.append(TextNode(delimited_content, text_type))
-
+            
+            # Update current_position to continue after the closing delimiter
+            current_position = closing_index + len(delimiter)
 
     return result
